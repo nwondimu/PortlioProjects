0f837fab024e25745a3865f6deb4c38ce477ddc6
nwondimu <142271077+nwondimu@users.noreply.github.com>
2023-09-27 17:15:35 -0400

    Update Covid 19 Portfolio Project.sql

--- Covid 19 Portfolio Project.sql
+++ Covid 19 Portfolio Project.sql
@@ -1,4 +1,4 @@
---Covid 19 Data Exploration 
+--Covid-19 Project 
 --Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 Source: https://ourworldindata.org/covid-deaths
 
